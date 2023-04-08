aecea3b54b64d09df466d754cf0393d4888ccba5
nguyentrungne <114131139+nguyentrungne@users.noreply.github.com>
2023-04-09 01:43:58 +0700

    first Commit

---
+++ BayMaxShop/BayMaxShop/Migrations/202304081751001_CreatedDatabase.cs
@@ -0,0 +1,252 @@
+﻿namespace BayMaxShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatedDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.tb_Adv",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 150),
+                        Description = c.String(maxLength: 500),
+                        Image = c.String(maxLength: 500),
+                        Link = c.String(maxLength: 500),
+                        Type = c.Int(nullable: false),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        ModifiedBy = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_Category",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 150),
+                        Description = c.String(),
+                        SeoTitle = c.String(),
+                        SeoDescription = c.String(maxLength: 250),
+                        SeoKeywords = c.String(maxLength: 150),
+                        Position = c.Int(nullable: false),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        ModifiedBy = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_New",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Image = c.String(),
+                        Title = c.String(nullable: false, maxLength: 150),
+                        Description = c.String(),
+                        Detail = c.String(),
+                        CategoryId = c.Int(nullable: false),
+                        SeoTitle = c.String(),
+                        SeoDescription = c.String(),
+                        SeoKeywords = c.String(),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        ModifiedBy = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.tb_Category", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.tb_Posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Image = c.String(),
+                        Title = c.String(nullable: false, maxLength: 150),
+                        Description = c.String(),
+                        Detail = c.String(),
+                        CategoryId = c.Int(nullable: false),
+                        SeoTitle = c.String(),
+                        SeoDescription = c.String(),
+                        SeoKeywords = c.String(),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        ModifiedBy = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.tb_Category", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.tb_Contact",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Email = c.String(maxLength: 50),
+                        Website = c.String(),
+                        Messsage = c.String(maxLength: 4000),
+                        IsRead = c.Boolean(nullable: false),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        ModifiedBy = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_OrderDetail",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        OrderId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        CustomerId = c.Int(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.tb_Order", t => t.OrderId, cascadeDelete: true)
+                .ForeignKey("dbo.tb_Product", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.OrderId)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.tb_Order",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Code = c.String(nullable: false),
+                        CustomerName = c.String(nullable: false),
+                        Phone = c.String(nullable: false),
+                        Address = c.String(nullable: false),
+                        TotalAmount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Quantity = c.Int(nullable: false),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        ModifiedBy = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_Product",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Image = c.String(),
+                        Title = c.String(nullable: false, maxLength: 150),
+                        ProductCode = c.String(),
+                        Description = c.String(),
+                        Detail = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        PriceSale = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Quantity = c.Int(nullable: false),
+                        IsHome = c.Boolean(nullable: false),
+                        IsFeature = c.Boolean(nullable: false),
+                        IsHot = c.Boolean(nullable: false),
+                        IsSale = c.Boolean(nullable: false),
+                        ProductCategoryId = c.Int(nullable: false),
+                        SeoTitle = c.String(),
+                        SeoDescription = c.String(),
+                        SeoKeywords = c.String(),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        ModifiedBy = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.tb_ProductCategory", t => t.ProductCategoryId, cascadeDelete: true)
+                .Index(t => t.ProductCategoryId);
+            
+            CreateTable(
+                "dbo.tb_ProductCategory",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 150),
+                        Description = c.String(),
+                        Icon = c.String(),
+                        SeoTitle = c.String(),
+                        SeoDescription = c.String(maxLength: 250),
+                        SeoKeywords = c.String(maxLength: 150),
+                        CreatedBy = c.String(),
+                        CreatedDate = c.DateTime(nullable: false),
+                        ModifiedDate = c.DateTime(nullable: false),
+                        ModifiedBy = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_ProductImage",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                        Image = c.String(),
+                        IsDefault = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_Subscribe",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Email = c.String(nullable: false),
+                        CreatedDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.tb_SystemSetting",
+                c => new
+                    {
+                        SettingKey = c.String(nullable: false, maxLength: 50),
+                        SettingValue = c.String(maxLength: 4000),
+                        SettingDescription = c.String(maxLength: 4000),
+                    })
+                .PrimaryKey(t => t.SettingKey);
+            
+            AddColumn("dbo.AspNetUsers", "FullName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "Phone", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.tb_OrderDetail", "ProductId", "dbo.tb_Product");
+            DropForeignKey("dbo.tb_Product", "ProductCategoryId", "dbo.tb_ProductCategory");
+            DropForeignKey("dbo.tb_OrderDetail", "OrderId", "dbo.tb_Order");
+            DropForeignKey("dbo.tb_Posts", "CategoryId", "dbo.tb_Category");
+            DropForeignKey("dbo.tb_New", "CategoryId", "dbo.tb_Category");
+            DropIndex("dbo.tb_Product", new[] { "ProductCategoryId" });
+            DropIndex("dbo.tb_OrderDetail", new[] { "ProductId" });
+            DropIndex("dbo.tb_OrderDetail", new[] { "OrderId" });
+            DropIndex("dbo.tb_Posts", new[] { "CategoryId" });
+            DropIndex("dbo.tb_New", new[] { "CategoryId" });
+            DropColumn("dbo.AspNetUsers", "Phone");
+            DropColumn("dbo.AspNetUsers", "FullName");
+            DropTable("dbo.tb_SystemSetting");
+            DropTable("dbo.tb_Subscribe");
+            DropTable("dbo.tb_ProductImage");
+            DropTable("dbo.tb_ProductCategory");
+            DropTable("dbo.tb_Product");
+            DropTable("dbo.tb_Order");
+            DropTable("dbo.tb_OrderDetail");
+            DropTable("dbo.tb_Contact");
+            DropTable("dbo.tb_Posts");
+            DropTable("dbo.tb_New");
+            DropTable("dbo.tb_Category");
+            DropTable("dbo.tb_Adv");
+        }
+    }
+}
---
+++ BayMaxShop/BayMaxShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BayMaxShop.Startup))]
+namespace BayMaxShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
